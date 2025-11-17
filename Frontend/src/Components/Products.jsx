9ab239e5f15509e869de0d8a41f9ff5c5b73f31c
Mani Yadegari <maniyadegari8478@gmail.com>
2025-11-17 21:09:53 +0330

    Fix Images

--- Frontend/src/Components/Products.jsx
+++ Frontend/src/Components/Products.jsx
@@ -10,8 +10,8 @@ export default function Products({ quantities, setQuantities }) {
   const [isCartModalOpen, setIsCartModalOpen] = useState(false);
   const cartRef = useRef(null);
 
-  const API_URL = "https://gelatocafe.ir/api"; // مسیر API
-  const BASE_URL = "https://gelatocafe.ir/images"; // مسیر تصاویر از backend
+  const API_URL = "https://gelatocafe.ir/api"; // برای درخواست API
+  const BASE_URL = "https://gelatocafe.ir"; // برای تصاویر
 
   // افزایش تعداد
   const addBtn = (id) =>
@@ -40,6 +40,7 @@ export default function Products({ quantities, setQuantities }) {
           grouped[p.category].push(p);
         });
 
+        // محصولات ناموجود به آخر
         Object.keys(grouped).forEach((cat) => {
           grouped[cat].sort((a, b) =>
             a.available === false ? 1 : b.available === false ? -1 : 0
@@ -48,7 +49,7 @@ export default function Products({ quantities, setQuantities }) {
 
         setCategories(grouped);
       } catch (err) {
-        console.error("خطا در دریافت محصولات:", err);
+        console.error(err);
       }
     };
     fetchProducts();
@@ -67,9 +68,13 @@ export default function Products({ quantities, setQuantities }) {
       const maxScroll = footer ? footer.offsetTop - cartHeight - 20 : Infinity;
       const scrollY = window.scrollY;
 
-      if (scrollY >= maxScroll) setCartPosition("stopped");
-      else if (scrollY >= 450) setCartPosition("fixed");
-      else setCartPosition("default");
+      if (scrollY >= maxScroll) {
+        setCartPosition("stopped");
+      } else if (scrollY >= 450) {
+        setCartPosition("fixed");
+      } else {
+        setCartPosition("default");
+      }
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -115,10 +120,7 @@ export default function Products({ quantities, setQuantities }) {
                   }`}
                   onClick={() => setSelectedProduct(elem)}
                 >
-                  <img
-                    src={`${BASE_URL}/${elem.image.split("/").pop()}`}
-                    alt={elem.name}
-                  />
+                  <img src={`${BASE_URL}${elem.image}`} alt={elem.name} />
                   <div>
                     <h3>{elem.name}</h3>
                     {elem.available ? (
@@ -175,7 +177,7 @@ export default function Products({ quantities, setQuantities }) {
               ✖
             </button>
             <img
-              src={`${BASE_URL}/${selectedProduct.image.split("/").pop()}`}
+              src={`${BASE_URL}${selectedProduct.image}`}
               alt={selectedProduct.name}
             />
             <h2>{selectedProduct.name}</h2>
